d30f7febd87e654b5391fe742fd4c112045de017
הילה  זילבר <hila@h-MacBook-Pro-sl-hylh.local>
2025-11-27 00:20:57 +0200

    1st p

--- Primes.java
+++ Primes.java
@@ -1,5 +1,30 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int index = Integer.parseInt(args[0]);
+        boolean[] prime = new boolean[index + 1];
+        prime[0] = false;
+        prime[1] = false;
+        for (int i = 2; i < index +1; i++){
+            prime[i] = true;
+        }
+
+        for (int i = 2; i <= Math.sqrt(index); i++){
+            if (prime[i]){
+                for (int j = i*2; j < index + 1; j += i){
+                    prime [j] = false;
+                    }
+                }
+            }
+        
+        int count = 0;
+        for (int i = 0; i < index +1; i++){
+            if (prime[i]){
+                count ++;
+                System.out.println(i);
+            }
+        }
+        int p = 100 * count / (index);
+        System.out.printf("There are %d primes between 2 and %d (%d%% are primes)", count,index, p);
+
     }
 }
